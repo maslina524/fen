9b0f9777b11583341f7b5dd9c8eda22f4d80a160
maslina524 <luk.kirill2804@gmail.com>
2026-07-03 17:21:03 +0300

    Added #![no_std]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[cfg(not(test))]
+mod panic_impl {
+    use core::panic::PanicInfo;
+
+    #[panic_handler]
+    fn panic(_info: &PanicInfo) -> ! {
+        loop {}
+    }
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn main() -> i32 {
+    0
 }

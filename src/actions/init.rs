c7a320dffeb777dac47198243f932ca21e5b0177
maslina524 <luk.kirill2804@gmail.com>
2026-07-04 22:07:04 +0300

    Now init action creates HEAD and description files

--- src/actions/init.rs
+++ src/actions/init.rs
@@ -1,13 +1,29 @@
+use alloc::format;
+use alloc::string::ToString;
+
 use crate::NoResult;
-use crate::os::fs::{self, FsError};
+use crate::os::error::ErrorType;
+use crate::os::fs;
 
 pub fn init() -> NoResult {
-    if let Err(e) = fs::create_dir(".git") {
-        return match e {
-            FsError::DirAlreadyExists => Err("git already initialized in this directory".into()),
+    if let Err(e) = fs::create_dir(".git", true) {
+        return match e.typ() {
+            ErrorType::DirAlreadyExists => Err("git already initialized in this directory".into()),
             _ => Err("an unexpected error occurred while creating the git directory".into())
         };
     }
+
+    // HEAD
+    let content = b"ref: refs/heads/master\n";
+    if let Err(e) = fs::create_file(".git/HEAD", content, content.len()) {
+        return Err(format!("{}", e.to_string()).into());
+    };
+
+    // description
+    let content = b"Unnamed repository; edit this file 'description' to name the repository.\n";
+    if let Err(e) = fs::create_file(".git/description", content, content.len()) {
+        return Err(format!("{}", e.to_string()).into());
+    };
 
     Ok(())
 }

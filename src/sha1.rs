6540d7462cbaf5b6a6f50f1727193517626bb2f6
maslina524 <luk.kirill2804@gmail.com>
2026-07-05 19:05:49 +0300

    Added method process_block & hex for Sha1

--- src/sha1.rs
+++ src/sha1.rs
@@ -1,4 +1,5 @@
 use alloc::format;
+use alloc::string::String;
 use alloc::vec::Vec;
 
 /// reference: https://github.com/pcaro90/Python-SHA1/blob/master/SHA1.py
@@ -8,19 +9,29 @@ pub struct Sha1 {
 
 impl Sha1 {
     pub fn new() -> Self {
-        Self { bytes: [0u32; 5] }
-    }
-
-    pub fn encrypt(&mut self, content: &[u8]) {
-        let stream = self.padding(Vec::new());
-        let stream = self.prepare(&stream);
-
-        for block in stream {
-            // self.process_block(block);
+        Self {
+            bytes: [
+                0x67452301,
+                0xEFCDAB89,
+                0x98BADCFE,
+                0x10325476,
+                0xC3D2E1F0,
+            ]
         }
     }
 
-    fn padding(&self, mut stream: Vec<u8>) -> Vec<u8> {
+    pub fn encrypt(&mut self, content: &[u8]) {
+        let stream = self.padding(content);
+        let stream = self.prepare(&stream);
+
+        for block in stream {
+            self.process_block(block);
+        }
+    }
+
+    fn padding(&self, stream: &[u8]) -> Vec<u8> {
+        let mut stream = Vec::from(stream);
+
         let l = stream.len();
         let hex_str = format!("{:0>16x}", l * 8);
         let mut hl = [0u8; 8];
@@ -62,15 +73,52 @@ impl Sha1 {
     }
 
     fn rotl(&self, n: u32, x: u32, w: u32) -> u32 {
-        (x << n) | (x >> w - n) 
+        (x << n) | (x >> (w - n)) 
     }
 
-    // fn process_block(&mut self, block: [u32; 16]) {
-    //     let mask  = u32::MAX;
+    fn process_block(&mut self, block: [u32; 16]) {
+        const MASK: u32 = u32::MAX;
 
-    //     let mut w = Vec::from(block);
-    //     for t in 16..80 {
-    //         w.push()
-    //     }
-    // }
+        let mut w = Vec::from(block);
+        for t in 16..80 {
+            let value = self.rotl(
+                1, 
+                w[t - 3] ^ w[t - 8] ^ w[t - 14] ^ w[t - 16], 
+                32
+            ) & MASK;
+            w.push(value);
+        }
+
+        let [mut a, mut b, mut c, mut d, mut e] = self.bytes;
+
+        for t in 0..80 {
+            let (k, f) = if t <= 19 {
+                (0x5a827999, (b & c) ^ (!b & d))
+            } else if t <= 39 {
+                (0x6ed9eba1, b ^ c ^ d)
+            } else if t <= 59 {
+                (0x8f1bbcdc, (b & c) ^ (b & d) ^ (c & d))
+            } else {
+                (0xca62c1d6, b ^ c ^ d)
+            };
+
+            let big_t = (self.rotl(5,a, 32) + f + e + k + w[t]) & MASK;
+            e = d;
+            d = c;
+            c = self.rotl(30, b, 32) & MASK;
+            b = a;
+            a = big_t;
+        }
+
+        self.bytes[0] = (a + self.bytes[0]) & MASK;
+        self.bytes[1] = (b + self.bytes[1]) & MASK;
+        self.bytes[2] = (c + self.bytes[2]) & MASK;
+        self.bytes[3] = (d + self.bytes[3]) & MASK;
+        self.bytes[4] = (e + self.bytes[4]) & MASK;
+    }
+
+    pub fn hex(&self) -> String {
+        let [a, b, c, d, e] = self.bytes;
+        format!("{a:08x}{b:08x}{c:08x}{d:08x}{e:08x}")
+    }
 }

f675e511a7c32547a94ebfb85f68f7895807fe86
maslina524 <luk.kirill2804@gmail.com>
2026-07-12 21:14:54 +0300

    Added fns inflate_block_no_compression & inflate_block_fixed_huffman

--- src/zlib/decompress.rs
+++ src/zlib/decompress.rs
@@ -1,6 +1,40 @@
 use alloc::vec::Vec;
+use alloc::vec;
 
 use crate::zlib::stream::Stream;
+
+pub fn inflate_block_no_compression(mut stream: Stream, inflated: &mut Vec<u8>) {
+    let len = stream.read_bytes(2);
+	// let nlen = stream.read_bytes(2);
+
+	for _ in 0..len {
+        inflated.push(stream.read_byte())
+    }
+}
+
+pub fn inflate_block_fixed_huffman(mut stream: Stream, inflated: &mut Vec<u8>) {
+    let mut bl_list = Vec::new();
+    
+    for _ in 0..144 {
+        bl_list.push(8);
+    }
+    for _ in 144..256 {
+        bl_list.push(9);
+    }
+    for _ in 256..280 {
+        bl_list.push(7);
+    }
+    for _ in 280..288 {
+        bl_list.push(8);
+    }
+
+    let literal_len_tree = huffman_tree_from_alphabet_and_bl_list(0..256, bl_list);
+
+    bl_list = vec![5; 30];
+    let distance_tree = huffman_tree_from_alphabet_and_bl_list(0..30, bl_list);
+
+    inflate_compressed_block(stream, literal_len_tree, distance_tree, &mut inflated)
+}
 
 fn inflate(mut stream: Stream) -> Vec<u8> {
     let mut bfinal = 0;

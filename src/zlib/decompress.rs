2a2d95de0a687afe07deaecbf0778047ba72623a
maslina524 <luk.kirill2804@gmail.com>
2026-07-12 21:01:40 +0300

    Added fn inflate to decompress.rs

--- src/zlib/decompress.rs
+++ src/zlib/decompress.rs
@@ -2,6 +2,24 @@ use alloc::vec::Vec;
 
 use crate::zlib::stream::Stream;
 
+fn inflate(mut stream: Stream) -> Vec<u8> {
+    let mut bfinal = 0;
+    let mut inflated = Vec::new();
+
+    while bfinal == 0 {
+        bfinal = stream.read_bit();
+        let btype = stream.read_bits(2);
+
+        match btype {
+            0b00 => inflate_block_no_compression(stream, &mut inflated),
+            0b01 => inflate_block_fixed_huffman(stream, &mut inflated),
+            0b10 => inflate_block_dynamic_huffman(stream, &mut inflated),
+        }
+    }
+
+    inflated
+}
+
 pub fn decompress(bytes: Vec<u8>, buf: &mut Vec<u8>) -> u32 {
     let mut stream = Stream::new(bytes);
 

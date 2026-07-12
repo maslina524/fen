6e420306e2b5f6452c86153803b743218841383f
maslina524 <luk.kirill2804@gmail.com>
2026-07-12 20:00:17 +0300

    New folder zlib & module stream.rs

---
+++ src/zlib/stream.rs
@@ -0,0 +1,52 @@
+use alloc::vec::Vec;
+
+pub struct Stream {
+    memory: Vec<u8>,
+    byte_idx: usize,
+    bit_idx: usize
+}
+
+impl Stream {
+    pub fn new(memory: Vec<u8>) -> Self {
+        Self { memory, byte_idx: 0, bit_idx: 0 }
+    }
+
+    pub fn read_byte(&mut self) -> u8 {
+        if self.bit_idx > 0 {
+            self.bit_idx = 0;
+            self.byte_idx += 1;
+        }
+
+        let byte = self.memory[self.byte_idx];
+        self.byte_idx += 1;
+        byte
+    }
+
+    pub fn read_bytes(&mut self, count: usize) -> u64 {
+        let mut bytes = 0u64;
+        for i in 0..count {
+            bytes |= (self.read_byte() as u64) << (8 * i)
+        }
+        bytes
+    }
+
+    pub fn read_bit(&mut self) -> u8 {
+        let bit = (self.memory[self.byte_idx] >> self.bit_idx) & 1;
+        self.bit_idx += 1;
+
+        if self.bit_idx > 7 {
+            self.bit_idx = 0;
+            self.byte_idx += 1;
+        }
+
+        bit
+    }
+
+    pub fn read_bits(&mut self, count: usize) -> u64 {
+        let mut bits = 0;
+        for i in 0..count {
+            bits |= (self.read_bit() as u64) << i;
+        }
+        bits
+    }
+}
